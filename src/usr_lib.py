07cd76a98c5db8f452ee74efad8d2b92d3d2cbfd
hyugai <huyle210504@gmail.com>
2024-11-27 15:06:16 +0700

    m

---
+++ src/usr_lib.py
@@ -0,0 +1,21 @@
+# playwright
+from playwright.sync_api import Playwright, Page, async_playwright
+from playwright.async_api import async_playwright
+
+# fake-useragent
+from fake_useragent import UserAgent
+
+# data structures
+import numpy as np
+import pandas as pd
+
+# database
+import sqlite3
+
+# asychronous
+import asyncio
+
+# others
+import os, re, sys
+
+HOMEPAGE = "https://batdongsan.com.vn/"
